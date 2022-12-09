154235b79f2602c728974b73f9a67ef3f1724c5a
Travis Mundy <mundyt4@mymacewan.ca>
2022-12-08 20:32:20 -0700

    Make the bootloader more standardized

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ So I am creating this repo to document my notes along with my actual code.
 need qemu-system-x86_64
 
 <h3>run the current boot command</h3>
+nasm -f bin ./boot.asm -o ./boot.bin
 qemu-system-x86_64 -hda ./boot.bin
--- kernelCode/PracOS/boot.asm
+++ kernelCode/PracOS/boot.asm
@@ -1,7 +1,24 @@
-ORG 0x7c00
+ORG 0 
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
 start:
+    jmp 0x7c0:step2
+
+step2:
+    cli ;clear interrupts
+    mov ax, 0x7c0 ; can not move value to ds directly so move to ax first 
+    mov ds, ax ;move ax to data segment
+    mov es, ax ;move ax to extra segment
+    mov ax, 0x00 ;move 0 to ax
+    mov ss, ax;move stack segment to 0x00
+    mov sp, 0x7c00 ;stack pointer points to this address
+    sti ; enables interrupts
+
     mov si, message ;move the address of the label message to SI register
     call print
     jmp $ ;keeps jumping to itself 
@@ -15,7 +32,6 @@ print:
     je .done; jump to done if al = 0
     call print_char
     jmp .loop
-
 .done:
     ret
    
@@ -24,7 +40,7 @@ print_char:
     int 0x10 ;intterupt 10 allows video-teletype output
     ret ;returns
 
-message: db 'Welcome to my OS', 0
+message: db 'Welcome to my os', 0
 
 times 510-($ - $$) db 0 ;fills 510 bytes and pads the rest with 0 
 dw 0xAA55 ;x86 machines are little endian 
