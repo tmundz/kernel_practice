b335ebf92929cd282e738846baceeeae6b2e67d7
Travis Mundy <mundyt4@mymacewan.ca>
2022-12-29 23:20:01 -0700

    Add LSP

---
+++ kernelCode/cognitos/target/debug/deps/cognitos-6a81ef937d005d56.d
@@ -0,0 +1,5 @@
+/home/mundy/Documents/code/kernel/kernelCode/cognitos/target/debug/deps/cognitos-6a81ef937d005d56.rmeta: src/main.rs
+
+/home/mundy/Documents/code/kernel/kernelCode/cognitos/target/debug/deps/cognitos-6a81ef937d005d56.d: src/main.rs
+
+src/main.rs:
---
+++ kernelCode/cognitos/target/debug/deps/cognitos-7b47362c8e498b39.d
@@ -0,0 +1,5 @@
+/home/mundy/Documents/code/kernel/kernelCode/cognitos/target/debug/deps/cognitos-7b47362c8e498b39.rmeta: src/main.rs
+
+/home/mundy/Documents/code/kernel/kernelCode/cognitos/target/debug/deps/cognitos-7b47362c8e498b39.d: src/main.rs
+
+src/main.rs:
