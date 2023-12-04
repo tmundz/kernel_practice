ef0407269aa77d78f9608b3c8a8f3a8628224b20
Travis <mundyt4@mymacewan.ca>
2023-12-04 00:51:05 -0700

    UPdated heap

--- PracOS/src/memory/heap/heap.c
+++ PracOS/src/memory/heap/heap.c
@@ -1,7 +1,21 @@
 #include "heap.h"
 #include "kernel.h"
+#include "memory/memory.h"
 #include "status.h"
 #include <stdbool.h>
+
+// nmeed this because I am creating a table there could be better ways
+static int heap_validate_table(void *ptr, void *end, struct heap_table *table) {
+  int res = 0;
+  size_t table_size = (size_t)(end - ptr);
+  size_t total_blocks = table_size / PRACOS_HEAP_BLOCK_SIZE;
+  if (table->total != total_blocks) {
+    res = -EINVARG;
+    goto out;
+  }
+out:
+  return res;
+}
 
 static bool heap_validate_alignment(void *ptr) {
   return ((unsigned int)ptr % PRACOS_HEAP_BLOCK_SIZE) == 0;
@@ -15,6 +29,11 @@ int heap_create(struct heap *heap, void *ptr, void *end,
     res = -EINVARG;
     goto out;
   }
+
   memset(heap, 0, sizeof(struct heap));
-  heap->saddr = ptr;
+  heap->saddr = ptr; // start of the heap
+  heap->table = table;
+
+out:
+  return res;
 }
---
+++ PracOS/src/status.h
@@ -0,0 +1,8 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+#define PRACOS_ALL_OK 0
+#define EIO 1
+#define EINVARG 2
+
+#endif
